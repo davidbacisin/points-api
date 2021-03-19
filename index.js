f458e1207ea45200411fd8ea5a1d367a166394cb
David Bacisin <david@davidbacisin.com>
2021-03-18 20:06:54 -0500

    Add spend endpoint

--- index.js
+++ index.js
@@ -14,6 +14,83 @@ function mapGetOrInit(map, key, defaultValue) {
     return defaultValue
 }
 
+function transactionByTimestampAscending(a, b) {
+    if (a.timestamp > b.timestamp) return 1
+    else if (a.timestamp < b.timestamp) return -1
+    else return 0
+}
+
+function handleError(req, res, err) {
+    console.log(err)
+    res.status(500).send({
+        status: 'error',
+        message: 'An unexpected error occurred'
+    })
+}
+
+function addTransaction({ userPoints, payer, points, timestamp }) {
+    if (typeof payer !== 'string') {
+        return {
+            status: 'error',
+            message: `Parameter 'payer' must be a string`
+        }
+    }
+
+    const ts = Date.parse(timestamp)
+    if (isNaN(ts)) {
+        return {
+            status: 'error',
+            message: `Parameter 'timestamp' must be an ISO timestamp`
+        }
+    }
+
+    if (isNaN(points)) {
+        return {
+            status: 'error',
+            message: `Parameter 'points' must be a number`
+        }
+    }
+
+    userPoints.push({
+        payer,
+        points,
+        timestamp: ts
+    })
+
+    return {
+        error: null,
+        message: null
+    }
+}
+
+function createSpendingPlan({ userPoints, points }) {
+    // Sort those points in time order. Clone because sort is in-place
+    const clonedPoints = [...userPoints]
+    clonedPoints.sort(transactionByTimestampAscending)
+
+    // Spend those points. Note that we have to check all transactions 
+    // in case later transactions have actually spent points from earlier 
+    // transactions
+    let pointsLeftToSpend = points
+    const payers = new Map()
+    for (let i = 0; i < clonedPoints.length; i++) {
+        let transaction = clonedPoints[i]
+
+        // We're going to spend these points. Initialize the payer in the payers Map
+        if (!payers.has(transaction.payer))
+            payers.set(transaction.payer, 0)
+
+        // Spend only as many points as are available
+        // If transaction.points == 0, this will be a no-op
+        // If transaction.points < 0, this will increase pointsLeftToSpend. We'll have to spend them in a later transaction.
+        const pointsToSpendNow = Math.min(pointsLeftToSpend, transaction.points)
+        pointsLeftToSpend -= pointsToSpendNow
+        payers.set(transaction.payer, payers.get(transaction.payer) - pointsToSpendNow)
+    }
+
+    return payers
+}
+
 app.use(bodyParser.json())
 
 /**
@@ -26,27 +103,94 @@ app.get('/status', (req, res) => res.status(200).send({ status: 'OK' }))
  * Add a transaction for a specific payer and date
  */
 app.post('/points/user/:user/transaction', (req, res) => {
-    const userPoints = mapGetOrInit(pointStore, req.params.user, [])
-    userPoints.push({
-        payer: req.body.payer,
-        points: req.body.points,
-        timestamp: req.body.timestamp
-    })
-    res.status(200).send()
+    try {
+        const userPoints = mapGetOrInit(pointStore, req.params.user, [])
+
+        let addResult = addTransaction({
+            userPoints,
+            payer: req.body.payer,
+            points: req.body.points,
+            timestamp: req.body.timestamp
+        })
+
+        if (addResult.error) {
+            res.status(400).send(addResult.error)
+            return
+        }
+
+        res.status(201).send()
+    }
+    catch (err) {
+        handleError(req, res, err)
+    }
 })
 
 app.get('/points/user/:user/balances', (req, res) => {
-    const userPoints = mapGetOrInit(pointStore, req.params.user, [])
+    try {
+        const userPoints = mapGetOrInit(pointStore, req.params.user, [])
 
-    // Reduce points, matching on payer
-    let result = userPoints.reduce((acc, transaction) => {
-        // Default to 0 if it the payer hasn't been initialized yet
-        const currentSum = acc.get(transaction.payer) || 0
-        acc.set(transaction.payer, currentSum + transaction.points)
-        return acc
-    }, new Map())
+        // Reduce points, matching on payer
+        let result = userPoints.reduce((acc, transaction) => {
+            // Default to 0 if the payer hasn't been initialized yet
+            const currentSum = acc.get(transaction.payer) || 0
+            acc.set(transaction.payer, currentSum + transaction.points)
+            return acc
+        }, new Map())
 
-    res.status(200).send(Object.fromEntries(result.entries()))
+        res.status(200).send(Object.fromEntries(result.entries()))
+    }
+    catch (err) {
+        handleError(req, res, err)
+    }
+})
+
+app.post('/points/user/:user/spend', (req, res) => {
+    try {
+        const points = parseInt(req.body.points)
+        if (isNaN(points)) {
+            res.status(400).send({
+                status: 'error',
+                message: `Parameter 'points' is not a number`
+            })
+            return
+        }
+
+        const userPoints = mapGetOrInit(pointStore, req.params.user, [])
+        const plan = createSpendingPlan({
+            userPoints,
+            points
+        })
+
+        const pointsSpent = [...plan.values()].reduce((acc, p) => acc + p, 0)
+        if (-pointsSpent < points) {
+            res.status(409).send({
+                status: 'error',
+                message: 'Cannot spend more points than the user has available'
+            })
+            return
+        }
+
+        // Execute the spending plan
+        const summary = []
+        const timestamp = new Date().toISOString()
+        plan.forEach((points, payer) => {
+            addTransaction({
+                userPoints,
+                payer,
+                points,
+                timestamp
+            })
+
+            // Also populate the response object
+            summary.push({ payer, points })
+        })
+
+        // Spend was successful
+        res.status(200).send(summary)
+    }
+    catch (err) {
+        handleError(req, res, err)
+    }
 })
 
 app.listen(port, () => {
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,17 @@
 const { expect } = require('chai')
 const { default: Axios } = require('axios')
+const { v4: uuidv4 } = require('uuid');
 
 const requestOptions = {
     baseURL: 'http://localhost:3001/',
     validateStatus: (status) => status < 500,
 }
+
+/**
+ * Writing a new test? Use uuidv4() to generate a new user ID, therefore 
+ * allowing your tests to run independently of each other. Otherwise,
+ * you'll get side effects of adding/spending points from another test
+ */
 
 describe('Points API', function () {
     it('is alive', async function () {
@@ -14,15 +21,18 @@ describe('Points API', function () {
     })
 
     describe('Single points transaction', function () {
-        const user = 1
+        const user = uuidv4()
 
-        before('add a single transaction', async function () {
+        it('should add a single transaction', async function () {
             // Add points in 1 transaction
-            await Axios.post(`/points/user/${user}/transaction`, {
+            let r = await Axios.post(`/points/user/${user}/transaction`, {
                 payer: 'DANNON',
                 points: 100,
                 timestamp: new Date()
             }, requestOptions)
+
+            // A new transaction should be considered '201 Created'
+            expect(r.status).to.equal(201)
         })
 
         it('should return the number of points added per payer', async function () {
@@ -30,6 +40,171 @@ describe('Points API', function () {
             let r = await Axios.get(`/points/user/${user}/balances`, requestOptions)
             expect(r.status).to.equal(200)
             expect(r.data).to.have.property('DANNON', 100)
+        })
+    })
+
+    describe('Multiple transactions', function () {
+        const user = uuidv4()
+
+        before('add several transactions for several payers', async function () {
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: 100,
+                timestamp: new Date()
+            }, requestOptions)
+
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'UNILEVER',
+                points: 123,
+                timestamp: new Date()
+            }, requestOptions)
+
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'MILLER COORS',
+                points: 4000,
+                timestamp: new Date()
+            }, requestOptions)
+
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: 200,
+                timestamp: new Date()
+            }, requestOptions)
+
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'MILLER COORS',
+                points: -200,
+                timestamp: new Date()
+            }, requestOptions)
+        })
+
+        it('should sum all points by payer', async function () {
+            let r = await Axios.get(`/points/user/${user}/balances`, requestOptions)
+            expect(r.status).to.equal(200)
+            expect(r.data).to.have.property('DANNON', 300)
+            expect(r.data).to.have.property('UNILEVER', 123)
+            expect(r.data).to.have.property('MILLER COORS', 3800)
+        })
+    })
+
+    describe('Spending points with a negative transaction', function () {
+        const user = uuidv4()
+
+        before('add multiple transactions', async function () {
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: 300,
+                timestamp: '2020-10-31T10:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'UNILEVER',
+                points: 200,
+                timestamp: '2020-10-31T11:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: -100,
+                timestamp: '2020-10-31T15:00:00Z'
+            }, requestOptions)
+        })
+
+        it('should account for later negative transactions when spending earlier points', async function () {
+            /* If it doesn't account for later negative transactions, then 
+            spending 350 points would spend 300 from Dannon and 50 from Unilever.
+            This would result in ending balances of { Dannon: -100, Unilever: 150 }, 
+            which is not allowed. Instead, we expect the service to account for the 
+            -100 from Dannon before it tries to spend any of the points. */
+            let r = await Axios.post(`/points/user/${user}/spend`, {
+                points: 350
+            }, requestOptions)
+
+            expect(r.status).to.equal(200)
+            expect(r.data).to.be.an("Array")
+            expect(r.data).to.have.lengthOf(2)
+            expect(r.data).to.deep.include({ payer: 'DANNON', points: -200 })
+            expect(r.data).to.deep.include({ payer: 'UNILEVER', points: -150 })
+        })
+
+        it('should return the correct remaining balances', async function () {
+            let r = await Axios.get(`/points/user/${user}/balances`, requestOptions)
+            expect(r.status).to.equal(200)
+            expect(r.data).to.have.property('DANNON', 0)
+            expect(r.data).to.have.property('UNILEVER', 50)
+        })
+    })
+
+
+    describe('Multiple rounds of spending', function () {
+        const user = uuidv4()
+
+        before('add multiple transactions', async function () {
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: 1000,
+                timestamp: '2020-11-02T14:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'UNILEVER',
+                points: 200,
+                timestamp: '2020-10-31T11:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: -200,
+                timestamp: '2020-10-31T15:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'MILLER COORS',
+                points: 10000,
+                timestamp: '2020-11-01T14:00:00Z'
+            }, requestOptions)
+            await Axios.post(`/points/user/${user}/transaction`, {
+                payer: 'DANNON',
+                points: 300,
+                timestamp: '2020-10-31T10:00:00Z'
+            }, requestOptions)
+        })
+
+        it('should spend the oldest points first', async function () {
+            let r = await Axios.post(`/points/user/${user}/spend`, {
+                points: 5000
+            }, requestOptions)
+
+            expect(r.status).to.equal(200)
+            expect(r.data).to.be.an("Array")
+            expect(r.data).to.have.lengthOf(3)
+            expect(r.data).to.deep.include({ payer: 'DANNON', points: -100 })
+            expect(r.data).to.deep.include({ payer: 'UNILEVER', points: -200 })
+            expect(r.data).to.deep.include({ payer: 'MILLER COORS', points: -4700 })
+        })
+
+        it('should return the correct remaining balances', async function () {
+            let r = await Axios.get(`/points/user/${user}/balances`, requestOptions)
+            expect(r.status).to.equal(200)
+            expect(r.data).to.have.property('DANNON', 1000)
+            expect(r.data).to.have.property('UNILEVER', 0)
+            expect(r.data).to.have.property('MILLER COORS', 5300)
+        })
+
+        it('should handle multiple spends', async function () {
+            let r = await Axios.post(`/points/user/${user}/spend`, {
+                points: 5600
+            }, requestOptions)
+
+            expect(r.status).to.equal(200)
+            // No need for a type check, an earlier test checks that
+            expect(r.data).to.deep.include({ payer: 'MILLER COORS', points: -5300 })
+            expect(r.data).to.deep.include({ payer: 'DANNON', points: -300 })
+        })
+
+        it('should reject overspending', async function () {
+            let r = await Axios.post(`/points/user/${user}/spend`, {
+                points: 20000
+            }, requestOptions)
+
+            expect(r.status).to.equal(409)
+            expect(r.data).to.have.property('status', 'error')
+            expect(r.data).to.have.property('message')
         })
     })
 })
